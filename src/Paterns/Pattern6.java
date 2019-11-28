899866d29917b0d30e3af637d55112383b44c147
Kasun Aratthanage <kasunaratthanage@gmail.com>
2019-11-29 00:05:34 +0530

    Update Pattern6.java

--- src/Paterns/Pattern6.java
+++ src/Paterns/Pattern6.java
@@ -1,7 +1,7 @@
 package Paterns;
 
 public class Pattern6 {
-    public static void main(String ara [])
+    public static void main(String arg [])
     {
         System.out.println("Hi test git");
     }
